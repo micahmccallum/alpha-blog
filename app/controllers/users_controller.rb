d0f29b14a10721c72da4cb64c9a5aa2d42fbd1b4
micahmccallum <micahmccallum@outlook.com>
2019-12-24 13:08:07 -0500

    Fix various errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
     
     if @user.update(user_params)
       flash[:success] = "You have successfully updated your account"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'edit'
     end
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])  
   end
   def require_same_user
-    if current_user != @user.username
+    if current_user != @user
       flash[:danger] = "You can only edit your own account."
       redirect_to root_path
     end
